e424449e102e7c1c93ce369088a18798a066916a
Ruslan Gafurov <g.r.9@mail.ru>
2025-05-13 12:34:04 +0300

    added docstring

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
 
@@ -12,7 +12,9 @@ from main import main
 @patch('validators.input_data_validator.InputDataValidator.validate')
 @patch('reports.payout_report.PayoutReport.generate_report')
 @patch('reports.base_report.BaseReport.read_files')
-def test_main_payout_report(mock_read_files, mock_payout_report, mock_input_data_validator, mock_parse_args, file_paths, report_type):
+def test_main_payout_report(mock_read_files, mock_payout_report, mock_input_data_validator, mock_parse_args,
+                            file_paths, report_type):
+    """Тестирует функцию "main"."""
 
     mock_parse_args.return_value = MagicMock(files=file_paths, report=report_type)
     mock_input_data_validator.return_value = None
